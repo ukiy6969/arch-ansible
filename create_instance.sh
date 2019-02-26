c1669c01b6600dc3df987b6449095be2aab8e56d
ukiy6969 <ukiy6969@gmail.com>
2019-02-27 01:33:49 +0900

    update

--- create_instance.sh
+++ create_instance.sh
@@ -1,2 +1,5 @@
 gcloud compute instances create archlinux \
-      --image-project=arch-linux-gce --image-family=arch
+      --address=archlinux \
+      --machine-type=n1-standard-4 \
+      --image-project=arch-linux-gce \
+      --image-family=arch
--- start-arch-gcp.sh
+++ start-arch-gcp.sh
@@ -2,4 +2,5 @@
 
 gcloud config configurations activate home
 gcloud compute instances start archlinux
+sleep 5
 gcloud compute ssh archlinux -- -fNL 5902:localhost:5901
