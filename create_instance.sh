09bce09d57728c99bf1de49e046e21f093cf7f7b
ukiy6969 <ukiy6969@gmail.com>
2019-03-02 23:38:26 +0900

    change: boot disk size

--- create_instance.sh
+++ create_instance.sh
@@ -1,5 +1,6 @@
 gcloud compute instances create archlinux \
       --address=archlinux \
       --machine-type=n1-standard-4 \
+      --boot-disk-size=20GB \
       --image-project=arch-linux-gce \
       --image-family=arch
